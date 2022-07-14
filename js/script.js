b719d349c087de8ec1b20f697977050008cceac0
Marat <martin64@list.ru>
2022-07-14 05:04:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// 'use strict';
+
+// a = 5;
+// console.log(a);
+// // alert("Привет Маратка!");
+
+// // let number = 5;
+// // let string = "Hello!";
+// // let sym = Symbol();
+// // let boolean = true;
+// // //null;
+// // //undefined;
+// // let obj = {};
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log("2" === 2);
